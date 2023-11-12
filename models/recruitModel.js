09ffaeb9dd3d2c8258589aef110a5bfc9c93ce89
ywonchae1 <ywonchae62@gmail.com>
2023-11-12 20:20:54 +0900

    #15 feat: 워크샵 불참 사유 추가

--- models/recruitModel.js
+++ models/recruitModel.js
@@ -2,8 +2,10 @@ const db = require('../config/db.js');
 
 module.exports = {
     createRecruitForm: async(formData) => {
+        console.log(formData)
         const attend = formData.attend == 'true' ? true : false;
         const workshop = formData.workshop == 'true' ? true : false;
+        const reason = formData.reason;
         const personal_info = formData.personal_info == 'true' ? true : false;
         const deposit = formData.deposit == 'true' ? true : false;
 
@@ -31,21 +33,22 @@ module.exports = {
         const piro_level = formData.piro_level;
 
         // 입력 값 길이 제한 확인
-        if(email.length >= 32 || name.length >= 16 || university >= 24 || major >= 48 || minor >= 48 || address >= 300 || phone >= 24 || interview >= 24 || doyouknowpiro >= 48) {
+        if(reason.length >= 48 || email.length >= 32 || name.length >= 16 || university.length >= 24 || major.length >= 48 || minor.length >= 48 || address.length >= 300 || phone.length >= 24 || interview.length >= 24 || doyouknowpiro.length >= 48) {
             return {status: "fail"}
         }
         
         const rawQuery = `
         INSERT INTO Document(
-            attend_ok, workshop_ok, pi_ok, deposit_ok, email, name, gender, university, major, minor, minor_course, level, address, phone, interview, q1_introduce, q2_experience, q3_idea, q4_performance, q5_patience, q6_plan, coding_test, doyouknowpiro, piro_level)
+            attend_ok, workshop_ok, reason, pi_ok, deposit_ok, email, name, gender, university, major, minor, minor_course, level, address, phone, interview, q1_introduce, q2_experience, q3_idea, q4_performance, q5_patience, q6_plan, coding_test, doyouknowpiro, piro_level)
         VALUE(
-            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
         `;
+        
         try {
             await db.query(
                 rawQuery,
                 [
-                    attend, workshop, personal_info, deposit,
+                    attend, workshop, reason, personal_info, deposit,
                     email, name, gender, university, major, minor, course, level, address, phone, interview,
                     q1_introduce, q2_experience, q3_idea, q4_performance, q5_patience, q6_plan,
                     coding_test_fileDest, doyouknowpiro, piro_level
