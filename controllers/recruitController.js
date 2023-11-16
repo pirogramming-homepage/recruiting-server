9076f4c8f3f1cfc25049670ca630c1ecfc9abb96
ywonchae1 <ywonchae62@gmail.com>
2023-11-16 23:53:14 +0900

    #23 fix: nodemailer 안정화

--- controllers/recruitController.js
+++ controllers/recruitController.js
@@ -5,6 +5,8 @@ const inlineBase64 = require('nodemailer-plugin-inline-base64');
 const nodemailerHtml = require('../config/recruitForm.js');
 const dateCheck = require('../utils/dateCheck.js');
 const inlineCss = require('nodemailer-juice');
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
 
 // 파이썬 파일 업로드
 const multer = require('multer');
@@ -21,15 +23,14 @@ const upload = multer({ storage: storage }).array('coding-test');
 
 module.exports = {
     saveForm: async (req, res) => {
-        if(dateCheck.dateCheck()) {
+        if (dateCheck.dateCheck()) {
             const formData = req.body;
             const result = await recruitModel.createRecruitForm(formData);
             return res.send(result);
         }
-        return res.send({'result': ''});
     },
     saveFile: async (req, res) => {
-        if(dateCheck.dateCheck()) {
+        if (dateCheck.dateCheck()) {
             upload(req, res, err => {
                 if (err instanceof multer.MulterError) {
                     // A Multer error occurred when uploading.
@@ -47,48 +48,72 @@ module.exports = {
                 }
             });
         }
-        return res.send({ 'status': false });
     },
     sendMail: async (req, res) => {
-        if(dateCheck.dateCheck()) {
+        if (dateCheck.dateCheck()) {
+            const oauth2Client = new OAuth2(
+                process.env.OAUTH_CLIENT_ID,
+                process.env.OAUTH_CLIENT_SECRET,
+                'https://developers.google.com/oauthplayground' // Redirect URL(Optional)
+            );
+
+            oauth2Client.setCredentials({
+                refresh_token: process.env.OAUTH_REFRESH_TOKEN
+            });
+
+            const accessToken = oauth2Client.getAccessToken();
+
             const mail_info = req.body;
             const emailAddress = mail_info.email;
             try {
                 // node mailer
                 const transporter = nodemailer.createTransport({
-                    host: "smtp.gmail.com",
+                    service: 'gmail',
+                    host: "smtp.google.com",
                     port: 587,
                     secure: true,
+                    debug: true,
+                    logger: true,
                     pool: true,
                     auth: {
                         type: "OAuth2",
-                        // user: process.env.PIRO_MAIL,
-                        // pass: process.env.NODE_MAILER,
                         user: process.env.OAUTH_USER,
                         clientId: process.env.OAUTH_CLIENT_ID,
                         clientSecret: process.env.OAUTH_CLIENT_SECRET,
                         refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+                        accessToken: accessToken
+                    },
+                    tls: {
+                        // do not fail on invalid certs
+                        rejectUnauthorized: false,
                     },
                 });
                 transporter.use('compile', inlineCss());
 
                 const copy = await nodemailerHtml.getHtmlForm(mail_info);
                 // console.log(copy);
-                
+
                 let mailOptions = {
                     from: process.env.PIRO_MAIL,
                     to: emailAddress,
                     subject: `[피로그래밍 ${process.env.PIRO_LEVEL}기] 지원서 사본`,
                     html: copy
                 };
-                const info = await transporter.sendMail(mailOptions);
+
+                transporter.on('idle', async () => {
+                    // send next messages from the pending queue
+                    while(transporter.isIdle()){
+                        await transporter.sendMail(mailOptions);
+                    }
+                });
+
+                // const info = await transporter.sendMail(mailOptions);
                 // console.log(info.messageId);
                 return res.json({ 'status': true });
-            } catch(error) {
+            } catch (error) {
                 console.log('recruit controller error!!!!', error);
                 return res.json({ 'status': false });
             }
         }
-        return res.json({ 'status': false });
     }
 }
