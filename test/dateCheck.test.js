872712d607a5336c195d10d4ef3d0560bea5a098
ywonchae1 <ywonchae62@gmail.com>
2023-11-19 01:18:13 +0900

    #25 fix: 접속 시간 확인 수정

--- test/dateCheck.test.js
+++ test/dateCheck.test.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 
 test('현재 날짜가 리크루팅 기한에 해당하는지 확인합니다', async () => {
   const result = await dateCheck.dateCheck();
-  expect(result).toEqual(false); // .env의 날짜와 toEqual의 true, false를 변경하며 확인해 보기
+  expect(result).toEqual(true); // .env의 날짜와 toEqual의 true, false를 변경하며 확인해 보기
 });
