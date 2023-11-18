a3f85b72506c9bfc82d4a285e6e57c6c3dd28c9a
ywonchae1 <ywonchae62@gmail.com>
2023-11-19 00:22:52 +0900

    #25 feat: 폼 홈페이지 저장 라우터 등록 제거

--- routers/recruitRouter.js
+++ routers/recruitRouter.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const RecruitController = require('../controllers/recruitController.js');
 
-router.post('/get_csrf', RecruitController.getCSRF);
-router.post('/save_home', RecruitController.saveHome);
 router.post('/save_form', RecruitController.saveForm);
 router.post('/save_file', RecruitController.saveFile);
 router.post('/send_mail', RecruitController.sendMail);
