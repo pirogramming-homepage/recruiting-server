3c3fff78ead88552f7772cd049b73c26f3e40342
ywonchae1 <ywonchae62@gmail.com>
2023-11-19 12:55:28 +0900

    #27 fix: 서류 마감 시간 수정

--- utils/dateCheck.js
+++ utils/dateCheck.js
@@ -2,7 +2,7 @@ module.exports = {
   dateCheck: async () => {
     const today = new Date();
     const recruitStartDate = new Date(Number(process.env.RECRUIT_YEAR), Number(process.env.RECRUIT_START_MONTH)-1, Number(process.env.RECRUIT_START_DAY))
-    const recruitEndDate = new Date(Number(process.env.RECRUIT_YEAR), Number(process.env.RECRUIT_END_MONTH)-1, Number(process.env.RECRUIT_END_DAY))
+    const recruitEndDate = new Date(Number(process.env.RECRUIT_YEAR), Number(process.env.RECRUIT_END_MONTH)-1, Number(process.env.RECRUIT_END_DAY), 23, 59, 0)
     
     console.log('today..', today);
     console.log('recruitStartDate..', recruitStartDate);
