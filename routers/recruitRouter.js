cb6f7ba292b707ea4f86464450441a88295cf800
ywonchae1 <ywonchae62@gmail.com>
2023-11-21 00:25:28 +0900

    #35 fix: 접속 시간 확인 구현

--- routers/recruitRouter.js
+++ routers/recruitRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const RecruitController = require('../controllers/recruitController.js');
 
+router.get('/check_time', RecruitController.checkTime);
 router.post('/save_form', RecruitController.saveForm);
 router.post('/save_file', RecruitController.saveFile);
 router.post('/send_mail', RecruitController.sendMail);
